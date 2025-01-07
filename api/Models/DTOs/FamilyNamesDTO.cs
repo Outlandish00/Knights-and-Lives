10f080af494e8933f1810d520df25f26373b2471
Tyler Parker <tyler.parker13@hotmail.com>
2025-01-06 23:05:53 -0600

    finised mood bars logic and styles, also started the new sim screen and got the logic done

---
+++ api/Models/DTOs/FamilyNamesDTO.cs
@@ -0,0 +1,8 @@
+public class FamilyNameDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsRoyalty { get; set; }
+    public bool IsWarrior { get; set; }
+    public bool IsPeasant { get; set; }
+}
--- client/src/scripts/apiManager.js
+++ client/src/scripts/apiManager.js
@@ -2,3 +2,8 @@ export const getAllYearlyEvents = async () => {
   const res = await fetch("/api/yearlyevents");
   return res.json();
 };
+
+export const getAllFamilyNames = async () => {
+  const res = await fetch("/api/familynames");
+  return res.json();
+};
